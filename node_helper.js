49a7dc373e9e1266f31e0adefa49a140bba0bad6
Andrew Birnberg <andrewbirnberg@Andrews-MacBook-Air.local>
2019-05-26 00:29:08 -0700

    basic node helper file

---
+++ node_helper.js
@@ -0,0 +1,11 @@
+const NodeHelper = require('node_helper')
+
+module.exports = NodeHelper.create({
+    start: function() {},
+
+    stop: function() {},
+
+    socketNotificationReceived(notification, payload) {},
+
+
+})
