7242785d1815b4a9a9e2721d630d20e1d1d0e838
Andrew Birnberg <andrewbirnberg@Andrews-MacBook-Air.local>
2019-05-26 00:24:58 -0700

    stub of module

---
+++ holomew.js
@@ -0,0 +1,17 @@
+Module.register('holomew', {
+    requiresVersion: '2.1.0',
+
+    defaults: {},
+
+    getStyles: {},
+
+    socketNotificationReceived: function(notification, payload) {},
+
+    notificationReceived: function(notification, payload, sender) {},
+
+    getDom: function() {},
+
+    start: function() {},
+
+    stop: function() {},
+})
